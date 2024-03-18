7ceedc7643f67cc18194388134a15fedd04885e8
lalapizco <v-lalapizco@expediagroup.com>
2024-03-18 17:34:13 -0600

    lalapizco-producers Create first producer

---
+++ kafka-basics/src/main/java/com/lalapizco/demo/kafka/ProducerDemo.java
@@ -0,0 +1,39 @@
+package com.lalapizco.demo.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+
+        Properties properties = getProducerProperties();
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello World");
+
+        //Send data
+        producer.send(producerRecord);
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+        producer.close();
+    }
+
+    private static Properties getProducerProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        return properties;
+    }
+}
